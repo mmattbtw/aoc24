ad084a2d89d67269a48bc51926ad5ff72961ddb3
mmattbtw <matt@mmatt.net>
2024-12-03 23:40:57 -0600

    day 4 complete!
    okay honestly with part 1 exactly what I was going to do beacuse I had a minesweeper project in my cs class and like this was lowkey kinda like it, part 2 especially
    
    i got stuck with part 2 because i was trying to do what i was doing in part 1 but it just didn't work for some reason
    
    came across this comment while looking for help: https://www.reddit.com/r/adventofcode/comments/1h689qf/2024_day_4_solutions/m0brv3m/ much smarter then what i was going to do
    
    so i implemented a stringstream (sorta unfimiliar but I've heard the name before) and it worked perfectly!!!

---
+++ day0/part1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <regex>
+
+int main()
+{
+    // std::ifstream file("test.txt");
+    std::ifstream file("input.txt");
+
+    return 0;
+}
--- day4/part2.cpp
+++ day4/part2.cpp
@@ -1,41 +1,33 @@
+// shoutout https://www.reddit.com/r/adventofcode/comments/1h689qf/2024_day_4_solutions/m0brv3m/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
 #include <string>
 #include <regex>
-
 int main()
 {
-    std::ifstream file("test.txt");
-    std::vector<std::vector<char>> grid;
+    std::ifstream file("input.txt");
+    std::vector<std::string> grid;
     std::string line;
     while (std::getline(file, line))
     {
-        std::vector<char> row;
-        for (char c : line)
-        {
-            row.push_back(c);
-        }
-        grid.push_back(row);
+        grid.push_back(line);
     }
     int total = 0;
-    for (int row = 0; row < grid.size(); row++)
+    for (int row = 1; row < grid.size() - 1; row++)
     {
-        for (int col = 0; col < grid.at(row).size(); col++)
+        for (int col = 1; col < grid[row].length() - 1; col++)
         {
-            if (grid.at(row).at(col) == 'A')
-            {
-                // check up left for an 'M'
-                if (
-                    // check for out of range
+            std::stringstream left, right;
+            left << grid[row - 1][col - 1] << grid[row][col] << grid[row + 1][col + 1];
+            right << grid[row + 1][col - 1] << grid[row][col] << grid[row - 1][col + 1];
 
-                    grid.at(row - 1).at(col - 1) == 'M' &&
-                    grid.at(row - 1).at(col + 1) == 'M' &&
-                    grid.at(row + 1).at(col + 1) == 'S' &&
-                    grid.at(row + 1).at(col - 1) == 'S')
-                {
-                    total++;
-                }
+            if ((left.str() == "MAS" || left.str() == "SAM") &&
+                (right.str() == "MAS" || right.str() == "SAM"))
+            {
+                total++;
             }
         }
         std::cout << std::endl;
